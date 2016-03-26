9f262450469628c506468e714c83e99192871fe3
Jeanne Boyarsky <nyjeanne@gmail.com>
2016-03-26 19:41:09 -0400

    add end to end logic and access token (didn't commit real token)

--- src/main/java/jb/CheckForksForFileName.java
+++ src/main/java/jb/CheckForksForFileName.java
@@ -2,31 +2,59 @@ package jb;
 
 import java.util.*;
 
+import org.springframework.http.*;
 import org.springframework.web.client.*;
 
 import com.google.gson.*;
 
+/**
+ * Before running, go to https://github.com/settings/tokens/new and generate a
+ * personal access token. You don't need to check any boxes for special
+ * permissions. Copy that token under the TODO a few lines down.
+ * 
+ * @author jeanne
+ *
+ */
 public class CheckForksForFileName {
 
 	private List<String> forks;
 	private RestTemplate template;
+	private String userName;
+	private String repoName;
+	private String fileName;
 
-	public CheckForksForFileName() {
+	// TODO place your token here
+	String personalToken = "madeUpToken";
+
+	// -----------------------------------------------------------------------
+
+	public static void main(String... args) {
+		if (args.length != 3) {
+			System.out.println("Incorrect call: should be jb.CheckForksForFileName userName repoName targetFileName");
+			System.out.println("For example: jb.CheckForksForFileName boyarsky jforumCsrf Owasp.CsrfGuard.js");
+		} else {
+			CheckForksForFileName checker = new CheckForksForFileName(args[0], args[1], args[2]);
+			checker.printRepositoriesWithMatchingFile();
+		}
+	}
+
+	// -----------------------------------------------------------------------
+
+	public CheckForksForFileName(String userName, String repoName, String fileName) {
 		forks = new ArrayList<>();
 		template = new RestTemplate();
+		this.userName = userName;
+		this.repoName = repoName;
+		this.fileName = fileName;
 	}
 
 	/**
 	 * Get all forks of the requested repository
 	 * 
-	 * @param userName
-	 * @param repoName
-	 * @return
 	 */
-	public List<String> getForks(String userName, String repoName) {
+	public List<String> getForks() {
 		String url = "https://api.github.com/repos/" + userName + "/" + repoName + "/forks";
-
-		String jsonString = template.getForObject(url, String.class);
+		String jsonString = callRestApi(url);
 
 		JsonParser parser = new JsonParser();
 		JsonArray root = (JsonArray) parser.parse(jsonString);
@@ -43,19 +71,41 @@ public class CheckForksForFileName {
 	/**
 	 * Check whether the given file name is in the master of that fork
 	 * 
-	 * @param fork
-	 * @param string
-	 * @return
 	 */
-	public boolean isFileInMasterOfFork(String fork, String fileName) {
-		String url = "https://github.com/ddverni/jforumCsrf/blob/master/" + fileName;
+	public boolean isFileInMasterOfFork(String fork) {
+		String url = fork + "/blob/master/" + fileName;
+		// use URL format rather than REST API URL format
+		url = convertToBrowserUrl(url);
 
 		try {
-			template.getForObject(url, String.class);
+			callRestApi(url);
 		} catch (HttpClientErrorException e) {
 			return false;
 		}
 
 		return true;
+	}
+
+	private String convertToBrowserUrl(String url) {
+		return url.replace("https://api.github.com/repos", "https://github.com");
+	}
+
+	/**
+	 * Pass token because github rate limits requests
+	 */
+	private String callRestApi(String url) {
+		HttpHeaders headers = new HttpHeaders();
+
+		headers.add("Authorization", "token " + personalToken);
+
+		// ignore result because just testing connectivity
+		HttpEntity<String> request = new HttpEntity<String>(headers);
+		ResponseEntity<String> response = template.exchange(url, HttpMethod.GET, request, String.class);
+		return response.getBody();
+	}
+
+	private void printRepositoriesWithMatchingFile() {
+		List<String> forks = getForks();
+		forks.stream().filter(this::isFileInMasterOfFork).map(this::convertToBrowserUrl).forEach(System.out::println);
 	}
 }
--- src/test/java/jb/CheckForksForFileNameIT.java
+++ src/test/java/jb/CheckForksForFileNameIT.java
@@ -3,6 +3,7 @@ package jb;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
 
+import java.io.*;
 import java.util.*;
 
 import org.junit.*;
@@ -10,15 +11,27 @@ import org.junit.*;
 public class CheckForksForFileNameIT {
 
 	private CheckForksForFileName target;
+	private PrintStream originalSystemOut;
+	private ByteArrayOutputStream mockSystemOut;
 
 	@Before
-	public void setUp() {
-		target = new CheckForksForFileName();
+	public void redirectSystemOut() {
+		originalSystemOut = System.out;
+		mockSystemOut = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(mockSystemOut));
 	}
+
+	@After
+	public void restoreSystemOut() {
+		System.setOut(originalSystemOut);
+	}
+
+	// -------------------------------------------------------------------------------
 
 	@Test
 	public void listForks() {
-		List<String> actual = target.getForks("boyarsky", "jforumCsrf");
+		target = new CheckForksForFileName("boyarsky", "jforumCsrf", "test");
+		List<String> actual = target.getForks();
 		assertThat("fork list not empty", actual, hasSize(greaterThan(1)));
 		actual.forEach(this::assertFork);
 	}
@@ -28,17 +41,55 @@ public class CheckForksForFileNameIT {
 		assertThat("ends with api for repo name", actualFork, endsWith("/jforumCsrf"));
 	}
 
+	// -------------------------------------------------------------------------------
+
 	@Test
 	public void fileInFork() {
-		String fork = target.getForks("boyarsky", "jforumCsrf").get(0);
-		boolean actual = target.isFileInMasterOfFork(fork, "Owasp.CsrfGuard.js");
+		target = new CheckForksForFileName("boyarsky", "jforumCsrf", "Owasp.CsrfGuard.js");
+		String fork = target.getForks().get(0);
+		boolean actual = target.isFileInMasterOfFork(fork);
 		assertTrue("file found in fork", actual);
 	}
 
 	@Test
 	public void fileNotInFork() {
-		String fork = target.getForks("boyarsky", "jforumCsrf").get(0);
-		boolean actual = target.isFileInMasterOfFork(fork, "MadeUpFile");
+		target = new CheckForksForFileName("boyarsky", "jforumCsrf", "MadeUpFile");
+		String fork = target.getForks().get(0);
+		boolean actual = target.isFileInMasterOfFork(fork);
 		assertFalse("file not found in fork", actual);
+	}
+
+	// -------------------------------------------------------------------------------
+
+	@Test
+	public void notEnoughParameters() {
+		CheckForksForFileName.main("user", "repo");
+		assertValidationFails();
+	}
+
+	@Test
+	public void tooManyParameters() {
+		CheckForksForFileName.main("user", "repo", "targetFile", "other");
+		assertValidationFails();
+	}
+
+	private void assertValidationFails() {
+		String actualOutput = mockSystemOut.toString();
+		assertThat("should fail validation", actualOutput,
+				startsWith("Incorrect call: should be jb.CheckForksForFileName"));
+	}
+
+	// -------------------------------------------------------------------------------
+
+	@Test
+	public void endToEndForMatches() {
+		CheckForksForFileName.main("boyarsky", "jforumCsrf", "Owasp.CsrfGuard.js");
+		assertThat("should not be any output", mockSystemOut.toString(), startsWith("https://github.com"));
+	}
+
+	@Test
+	public void endToEndForNoMatches() {
+		CheckForksForFileName.main("boyarsky", "jforumCsrf", "NotAFile");
+		assertThat("should be no matches", mockSystemOut.toString(), isEmptyString());
 	}
 }
